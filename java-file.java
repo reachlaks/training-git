c37dd272e2cc7a32b8bfb484d20f63d987871ed8
reachlaks <reachlaks@hotmail.com>
2018-03-06 09:26:21 +0530

    Create java-file.java

---
+++ java-file.java
@@ -0,0 +1,18 @@
+package org.demo;
+ 
+import org.testng.annotations.Test;
+ 
+public class Sample {
+    @Test
+    public void doTest1(){
+        System.out.println("Testng test 1");
+    }
+    @Test
+    public void doTest2(){
+        System.out.println("Testng test 2");
+    }
+    @Test
+    public void doTest3(){
+        System.out.println("Testng test 3");
+    }   
+}
